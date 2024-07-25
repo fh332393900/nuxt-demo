63f5fb4abc7f56bf3f49fe66794e18aa63271563
fenghang <hang.feng@nx-engine.com>
2024-07-25 15:52:42 +0800

    init

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const count = ref<number>(0);
+
+count.value += 1;
+
+const getHello = async () => {
+  const res = await $fetch('/api/hello', {
+    method: 'POST'
+  });
+  console.log(res);
+};
+
+getHello();
+
+</script>
---
+++ server/api/login.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(() => {
+  const res = {
+    name: 's',
+    age: 22
+  };
+  return res;
+});
